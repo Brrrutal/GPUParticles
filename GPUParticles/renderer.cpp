3c0a5015fa091aad06e0f862bb9f0dad52eb2164
Serhiy Likhanov <slikhan@gmail.com>
2015-10-18 13:15:15 -0400

    DX renderer setup.

---
+++ GPUParticles/renderer.cpp
@@ -0,0 +1,11 @@
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+// Linking DX Libraries.
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+void InitializeRenderer()
+{
+}
