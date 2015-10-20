2c3b9e273eb3154d76b70993d36656421da5d68d
Serhiy Likhanov <slikhan@gmail.com>
2015-10-19 23:00:37 -0400

    Minor renaming, adding AdapterInfo structure.

--- GPUParticles/renderer.cpp
+++ GPUParticles/renderer.cpp
@@ -6,8 +6,18 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
-void InitializeRenderer(unsigned int screenWidth, unsigned int screenHeight)
+struct AdapterInfo
 {
+    unsigned int m_den;
+    unsigned int m_num;
+    unsigned long long m_videoMemoryMB;
+};
+
+bool GetAdapterInfo(unsigned int screenWidth, unsigned int screenHeight, AdapterInfo* pAdapterInfo)
+{
+    if (pAdapterInfo == NULL)
+        return false;
+
     HRESULT result;
     IDXGIFactory* pDXGIFactory;
     IDXGIAdapter* pDXGIAdapter;
@@ -16,54 +26,53 @@ void InitializeRenderer(unsigned int screenWidth, unsigned int screenHeight)
     UINT modeCnt;
     DXGI_MODE_DESC* pDXGIModeArray;
 
-    unsigned int den = 0;
-    unsigned int num = 1;
-
     // TODO: Fix memory leaks in COM-objects.
 
     result = CreateDXGIFactory(__uuidof(pDXGIFactory), (void**)&pDXGIFactory);
     if (FAILED(result))
-        return;
+        return false;
 
     result = pDXGIFactory->EnumAdapters(0, &pDXGIAdapter);
     if (FAILED(result))
-        return;
+        return false;
 
     result = pDXGIAdapter->EnumOutputs(0, &pDXGIOutput);
     if (FAILED(result))
-        return;
+        return false;
 
     result = pDXGIOutput->GetDisplayModeList(
             DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &modeCnt, NULL);
     if (FAILED(result))
-        return;
+        return false;
 
     pDXGIModeArray = new DXGI_MODE_DESC[modeCnt];
     result = pDXGIOutput->GetDisplayModeList(
             DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &modeCnt, pDXGIModeArray);
 
     if (FAILED(result))
-        return;
+        return false;
 
     for (UINT modeIdx = 0; modeIdx < modeCnt; ++modeIdx)
     {
         if (pDXGIModeArray[modeIdx].Width == screenWidth &&
             pDXGIModeArray[modeIdx].Height == screenHeight)
         {
-            den = pDXGIModeArray[modeIdx].RefreshRate.Denominator;
-            num = pDXGIModeArray[modeIdx].RefreshRate.Numerator;
+            pAdapterInfo->m_den = pDXGIModeArray[modeIdx].RefreshRate.Denominator;
+            pAdapterInfo->m_num = pDXGIModeArray[modeIdx].RefreshRate.Numerator;
         }
     }
     
     DXGI_ADAPTER_DESC adapterDesc;
     result = pDXGIAdapter->GetDesc(&adapterDesc);
     if (FAILED(result))
-        return;
+        return false;
 
-    unsigned long long videoMemoryMB = adapterDesc.DedicatedVideoMemory / 1024 / 1024;
+    pAdapterInfo->m_videoMemoryMB = adapterDesc.DedicatedVideoMemory / 1024 / 1024;
     
     delete[] pDXGIModeArray;
     pDXGIOutput->Release();
     pDXGIAdapter->Release();
     pDXGIFactory->Release();
+
+    return true;
 }
