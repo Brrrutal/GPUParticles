201bd248888f2147ccb98ec0b8332cb73b385e51
Serhiy Likhanov <slikhan@gmail.com>
2015-10-19 22:47:17 -0400

    Baby steps. Getting refresh rate from adapter, video memory limit.

--- GPUParticles/renderer.cpp
+++ GPUParticles/renderer.cpp
@@ -6,6 +6,64 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
-void InitializeRenderer()
+void InitializeRenderer(unsigned int screenWidth, unsigned int screenHeight)
 {
+    HRESULT result;
+    IDXGIFactory* pDXGIFactory;
+    IDXGIAdapter* pDXGIAdapter;
+    IDXGIOutput* pDXGIOutput;
+
+    UINT modeCnt;
+    DXGI_MODE_DESC* pDXGIModeArray;
+
+    unsigned int den = 0;
+    unsigned int num = 1;
+
+    // TODO: Fix memory leaks in COM-objects.
+
+    result = CreateDXGIFactory(__uuidof(pDXGIFactory), (void**)&pDXGIFactory);
+    if (FAILED(result))
+        return;
+
+    result = pDXGIFactory->EnumAdapters(0, &pDXGIAdapter);
+    if (FAILED(result))
+        return;
+
+    result = pDXGIAdapter->EnumOutputs(0, &pDXGIOutput);
+    if (FAILED(result))
+        return;
+
+    result = pDXGIOutput->GetDisplayModeList(
+            DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &modeCnt, NULL);
+    if (FAILED(result))
+        return;
+
+    pDXGIModeArray = new DXGI_MODE_DESC[modeCnt];
+    result = pDXGIOutput->GetDisplayModeList(
+            DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &modeCnt, pDXGIModeArray);
+
+    if (FAILED(result))
+        return;
+
+    for (UINT modeIdx = 0; modeIdx < modeCnt; ++modeIdx)
+    {
+        if (pDXGIModeArray[modeIdx].Width == screenWidth &&
+            pDXGIModeArray[modeIdx].Height == screenHeight)
+        {
+            den = pDXGIModeArray[modeIdx].RefreshRate.Denominator;
+            num = pDXGIModeArray[modeIdx].RefreshRate.Numerator;
+        }
+    }
+    
+    DXGI_ADAPTER_DESC adapterDesc;
+    result = pDXGIAdapter->GetDesc(&adapterDesc);
+    if (FAILED(result))
+        return;
+
+    unsigned long long videoMemoryMB = adapterDesc.DedicatedVideoMemory / 1024 / 1024;
+    
+    delete[] pDXGIModeArray;
+    pDXGIOutput->Release();
+    pDXGIAdapter->Release();
+    pDXGIFactory->Release();
 }
